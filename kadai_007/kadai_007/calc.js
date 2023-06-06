2c7263f8f33670ba24f3010ef0abf4ddc1e6ce58
Naooooooooooo <132202459+Naooooooooooo@users.noreply.github.com>
2023-06-06 23:08:34 +0900

    Add files via upload

---
+++ kadai_007/kadai_007/calc.js
@@ -0,0 +1,15 @@
+let num = 0;
+for(num = 0 ; num <= 0 ,num < 30 ; num++){
+
+if (num % 3 === 0 && num % 5 === 0){
+    console.log('3と5の倍数です');
+}
+else if(num % 3 === 0){
+    console.log('3の倍数です');
+}
+else if(num % 5 === 0){
+    console.log('5の倍数です');
+}
+else{console.log(num);}
+}
+
